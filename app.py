a1e5e580fe576641e91216dc12bd1469205f3624
m-hajizade <123562779+m-hajizade@users.noreply.github.com>
2025-04-07 11:01:15 +0330

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a strong secret key
 
 # Dummy credentials
-USERNAME = "mohamad"
+USERNAME = "derakhshan"
 PASSWORD = "123456"
 
 # Login route
